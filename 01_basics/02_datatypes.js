57583375eaea8dc68df835100485b3cf9ffb18a4
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-09-08 00:12:23 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log("Xander")
+
+let name = "Xander"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2^53
+// bigint
+// string = "",'' prefer ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => mostly used for uniqueness 
+
+// object 
+
+console.log(typeof "Xander")
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null);
+console.log(typeof undefined);
