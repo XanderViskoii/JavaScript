122be6350a5b881ddab8706df0f000a1c9d77a26
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-09-09 23:46:49 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,7 +30,7 @@ let stringNumber = String(SomeNumber)
 // console.log(typeof stringNumber);
 
 //*********************************  Operations *************************
-let value = 3
+/*let value = 3
 let negValue = -value
 console.log(negValue);
 
@@ -41,3 +41,24 @@ let str2 = " Xander"
 
 let str3 = str1 + str2
 console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(1 + "2" +2); //if string is first then all converted to string else first perform operation then convert to string
+
+
+console.log(((3 + 4) * 5) % 3); */
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
