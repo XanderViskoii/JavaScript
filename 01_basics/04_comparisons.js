fd697b1391e6707745b42c2fcb8b22595d62431c
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-09-09 23:47:03 +0000

    comparison

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0); //comparison operator and equality check operator here work differently i.e. comparison treat null as 0
+console.log(null >= 0); 
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===  Also checks datatype
+
+console.log("2" === 2);
