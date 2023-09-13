cf06370cb1562ce478b06de89053a731d714cf56
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-09-13 23:35:59 +0000

    Datees

---
+++ 01_basics/07_Dates.js
@@ -0,0 +1,49 @@
+// Dates
+
+let myDate = new Date()
+/*console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString()); 
+console.log(myDate.toJSON()); 
+console.log(myDate.toLocaleDateString('en-IN')); 
+console.log(myDate.toLocaleDateString('hi-IN')); 
+console.log(myDate.toLocaleString('hi-IN')); 
+console.log(myDate.toLocaleTimeString('hi-IN')); 
+console.log(myDate.toTimeString()); 
+console.log(myDate.toUTCString('en-IN')); 
+console.log(myDate.getTimezoneOffset());  //Universal coordinated time and local device minutes difference
+console.log(typeof myDate);
+let newDate = new Date(2023,0,23)
+console.log(newDate.toLocaleDateString('hi-IN')); 
+let newDate = new Date(2023, 0, 23, 23, 34)
+console.log(newDate.toLocaleString('hi-IN')); 
+let newDate = new Date("01-1-1965")
+// console.log(newDate.toLocaleString('hi-IN')); 
+
+let myTimeStamp = Date.now()
+//console.log(myTimeStamp);
+//console.log(newDate);
+//console.log(newDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newDate1 = new Date()
+console.log(newDate1.getMonth() + 1); //in JS Calendar has 0 -11 month , so for simplicity of user
+console.log(newDate1.getDay());
+
+console.log(`Hi, it's ${Date.now()} milliseconds passed from 1st january 1970 till now`);
+console.log(newDate.getMilliseconds()); */
+
+let newDate1 = new Date()
+console.log(newDate1.toLocaleString('default',{weekday: "long",timeZoneName:"longGeneric"}))
+
+
+
+
+
+
+
+
+
+
+
