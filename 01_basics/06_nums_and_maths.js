28398e7ed202e4086265dd946b1d06282063645b
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-09-13 00:08:54 +0000

    numbers & maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,37 @@
+/*const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2));
+
+const otherNum = 123.89664
+
+console.log(otherNum.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN')); */
+
+// ++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++
+
+/*console.log(Math);
+console.log(Math.abs(-49.8)); //Absolute value
+console.log(Math.round(4.786));
+console.log(Math.ceil(4.2)); //top
+console.log(Math.floor(4.9)); //Greatest Integer possible : Integer part
+console.log(Math.sqrt(24));
+console.log(Math.min(4,3,5,7));
+console.log(Math.max(4,3,5,7));*/
+
+console.log(Math.random()); //returs between 0 and 1
+console.log(Math.floor(Math.random()*10) + 1); //min value :1 all integers
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
+
+
