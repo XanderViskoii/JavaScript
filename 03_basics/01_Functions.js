b340857869b283aae8710881399e964cd4f9c619
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-09-17 23:09:07 +0000

    first function

---
+++ 03_basics/01_Functions.js
@@ -0,0 +1,19 @@
+
+function saymyName() {
+    console.log("X");
+    console.log("a");
+    console.log("n");
+    console.log("d");
+    console.log("e");
+    console.log("r");
+} 
+
+//saymyName()
+
+function addTwoNumbers(n1,n2){
+    n3 = n1 + n2
+    console.log(n3);
+}
+
+const result = addTwoNumbers(3, 6)
+console.log("Result: ", result);
