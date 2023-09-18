12117ff0fc878a4ec4d8f9310bc4131116cee52d
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-09-18 22:57:47 +0000

    Functions and parameter p1 complete

--- 03_basics/01_Functions.js
+++ 03_basics/01_Functions.js
@@ -10,10 +10,30 @@ function saymyName() {
 
 //saymyName()
 
-function addTwoNumbers(n1,n2){
-    n3 = n1 + n2
-    console.log(n3);
+// function addTwoNumbers(n1,n2){ //n1,n2 are parameters
+//      n1 + n2
+//     console.log(n1 + n2);
+// }
+function addTwoNumbers(n1,n2){ //n1,n2 are parameters
+    // let result = n1 + n2
+    // return result
+    return n1 + n2 
 }
+//Once return is used, function will do nothing
 
-const result = addTwoNumbers(3, 6)
-console.log("Result: ", result);
+const result = addTwoNumbers(3, 6) //3,6 are arguments
+//To store in a variable u shld reutrn (while worling in function otherwise it will be undefined)
+
+
+// console.log("Result: ", result);
+
+
+function LoginUserMessage(username = "Sam"){  //Sam is by default value
+    if(!username){
+        console.log("Please enter a username");
+        return 
+    }
+    return `${username} just logged in`
+}
+// console.log(LoginUserMessage("Xander"))
+console.log(LoginUserMessage())
