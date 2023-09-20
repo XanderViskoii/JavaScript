65b5d03e4154cf327e01c2e2b53d3373901d4c13
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-09-20 23:51:21 +0000

    execution

---
+++ 03_basics/execution_js.js
@@ -0,0 +1,13 @@
+let val1 = 10
+let val2 = 5
+function addNum(num1,num2){
+    let total = num1 + num2
+    return total
+}
+let result1 = addNum(val1,val2)
+let result2 = addNum(10,2)
+console.log(`value of result1 is ${result1} and result2 is ${result2}`);
+
+//Steps of Execution:
+//1. Global Execution --> it is first allocated in this . ye to hona hi hona hai
+//2. Memory Phase(allocation of memory) 
