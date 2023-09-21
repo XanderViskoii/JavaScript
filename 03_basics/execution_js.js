576a34cd3baaee86f3e656c6c2a93c4f6c467c0f
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-09-21 23:54:57 +0000

    comments

--- 03_basics/execution_js.js
+++ 03_basics/execution_js.js
@@ -9,7 +9,12 @@ let result2 = addNum(10,2)
 console.log(`value of result1 is ${result1} and result2 is ${result2}`);
 
 //Steps of Execution:
-//1. Global Execution --> it is first allocated in this . ye to hona hi hona hai
+//1. Global Execution --> it is first allocated in this.  (ye to hona hi hona hai)
 //2. Memory Phase(allocation of memory) 
-//3. Execution Phase
+//3. Execution Phase 
+/*   val1<--10
+     val2<--5
+     addNum --> a new seperate Execution Context(ec) is Created 
+     A new ec is created everytime you call a function */
 //4. Execution context
+// JS is single threaded
