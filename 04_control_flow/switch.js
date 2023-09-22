3b40b556c26055065046f4ca3246efb85ebeb0a9
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-09-22 23:39:43 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,47 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 13
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Februar");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("Septembe");
+        break;
+    case 10:
+        console.log("October");
+        break;
+
+    default:
+        console.log("Default case match");
+        break;
+}
