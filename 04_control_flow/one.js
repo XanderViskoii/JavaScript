f1972541dab22b7cd644c0433bc5d8646e38cefd
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-09-22 23:18:32 +0000

    if else complete

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,41 +1,70 @@
 // if
 
-if (true){
+/*if (condition){
+
+} //Agr condition is true to andr ka code execute karenge nahin to nahin karenge
+
+const isUserLoggedIn = true
+if (isUserLoggedIn){
 
 }
-//Agr condition is true then it will execute and go into the scope 
-//if false it will not go further
 
-// === also checks type along with magnitude
-// !== negative 
+// <, >, <=, >=, ==, ===(checks type also), !=, !==(negation of strict equality)
+
+if (3!=2){
+    
+} Since condition i.e. 3 is not equal to 2 holds itself that's why the internal code(line 15) will be executed
+if (2 === "2"){
+    console.log("Executed");
+} condition not true since one is String another is number*/
+
+const a = Number(2)
+const b = Number(2)
+if (a !== b){
+    console.log("Executed");
+} // !(a===b)
+
+const temperature = 41
+
+if (temperature<50){
+    console.log("temperature is less than 50");
+}
+else {
+    console.log("temperature is greater than 50");
+}
 
 const score = 200
-if (score>100){
+
+/*if (score>100){
     const power = "fly"
-    console.log(`User power: ${power}`);
+    console.log(`User Power: ${power}`);
 }
 
-//ShortHand Notation
-const balance = 1000
-if (balance>500) console.log("test"), console.log("test2");
+console.log(`User power: ${power}`); */
 
-if(balance < 500){
+/*const balance = 1000
+
+// if (balance>500) console.log("test"), console.log("test2");
+//implicit scope 
+if (balance<500){
     console.log("less than 500");
-}
-else if (balance <750){
-    console.log();
-}
+} else if (balance<750){
+    console.log("Less than 750");
+} else if (balance<900) {
+    console.log("Less than 900");
+} else {
+    console.log("Less than 1200");
+}*/
 
-const userLoggedIn = true
-const debitCard = true
-const loggedInFromGoogle = true
-const loggedInFromEmail = true
-
-if (userLoggedIn && debitCard){
+const userLoggeedIn = true
+const DebitCard = true
+const LoggedInFromGoogle = false
+const LoggedInFromEmail = true
+// && all conditions if true then only execution
+if (userLoggeedIn && DebitCard){
     console.log("Allow to buy course");
 }
-
-if (loggedInFromGoogle || loggedInFromEmail ) // this pipes is (||) or operator 
-{
-    console.log("User Logged In");
+// ||(or) any one if true condition executes the code
+if (userLoggeedIn || DebitCard){
+    console.log("User logged in");
 }
