27ae2f46155b92c6d9bc9d39ff77346c8634caa7
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-09-23 00:12:04 +0000

    Terniary Operator

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -39,3 +39,9 @@ val1 = null ?? 10 ?? 20
 
 console.log(val1);
 
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("Less than 80") : console.log("more than 80");
