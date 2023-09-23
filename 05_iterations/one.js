57523d9d9c8d80d9e9f8b1f6ec80b0437b193957
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-09-23 23:46:53 +0000

    break and continue

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -28,3 +28,21 @@ for (let i = 0; i < myArray.length; i++) {
     
 }
 
+// break and continue
+for (let i = 1; i <= 20; i++) {
+    if (i==5){
+        // console.log(`Detected 5 `);
+        break
+    }
+    // console.log(`Value of i is ${i}`);
+    
+}
+for (let i = 1; i <= 20; i++) {
+    if (i==5){
+        // console.log(`Detected 5 `);
+        continue
+    }
+    // console.log(`Value of i is ${i}`);
+    
+}
+
