d30291fd311c24a7fdad338dca144bba1045e8e6
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-09-24 00:06:50 +0000

    while loop & do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+
+
+// while (condition) {   }
+let index = 1
+while (index <= 10){
+    // console.log(`Value of index is ${index}`);
+    index = index*2
+}
+
+let myArray = ['Raadharani', 'Saubhadrey', 'krishnaey','Kauntey']
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr+1
+}
+
+let score = 11
+
+do {
+   console.log(`Score is ${score}`);
+   score++
+} while (score <= 10);
