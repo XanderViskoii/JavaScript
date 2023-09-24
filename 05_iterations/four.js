4a1583cfcd810f37ece8b549b38874a270b33fb9
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-09-24 23:44:56 +0000

    For In function

---
+++ 05_iterations/four.js
@@ -0,0 +1,11 @@
+const myObject = {
+    js: "JavaScript",
+    cpp: "C++",
+    RB: "ruby",
+    swift: "Swift by apple"
+}
+for (const key in myObject) {
+        console.log(myObject);
+        
+
+}
