8e2ac9c4337ba55c4ce8c209b60f739e0d92d47d
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-09-24 23:28:41 +0000

    MAP & for loop

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -1,10 +1,39 @@
 // for of loop
 
-// ["","",""]
-// [{},{},{}]
+// ["","",""]----> Array with string
+// [{},{},{}]----> Array with Objects
 
 const arr = [1, 2, 3, 4, 5]
 
-for (const iterator of object) {
-    
+for (const num of arr) {
+    //console.log(num);
 }
+
+const greetings = "Hello World!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`);
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN',"India")
+map.set('USA',"United states of america")
+map.set('FR',"France")
+
+//  console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key,':-',value);
+}
+
+const myObject = {
+    'game1': 'NFS',
+    'game2': 'Spiderman'
+}
+
+// for (const [key,value] of myObject) {
+//     console.log(key, ':-', value);
+// } For loop does not work on objects
+
+
