42eba36d43c3a0b1ab4e375305a468c218125231
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-09-25 22:42:57 +0000

    ForIn Loop

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -4,8 +4,28 @@ const myObject = {
     RB: "ruby",
     swift: "Swift by apple"
 }
+// ForIn Loop is used for objects
 for (const key in myObject) {
-        console.log(myObject);
-        
-
+    // console.log(key);
+    // console.log(myObject[key]);
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
 }
+
+const programming = ["js","rb","py","java","cpp"]
+
+for (const key in programming) {
+    // console.log(key);
+    // console.log(programming[key]);
+}
+
+/*const map = new Map()
+map.set('IN',"India")
+map.set('USA',"United states of america")
+map.set('FR',"France")
+
+for (const key in map) {
+    console.log(key);
+} Map Not Iteratable */
+
+
+
