97987337980cb9420851b224e2d97702c35fc8ba
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-10-07 23:59:51 +0000

    Don't know what is committed

--- 08_events/6_RandomColourGenerate/6.js
+++ 08_events/6_RandomColourGenerate/6.js
@@ -1,11 +1,11 @@
 const randomColor = function(){
     const hex = "0123456789ABCDEF";
     let color = '#';
-    for(let i = 0; i<i<6; i++){
+    for(let i = 0; i<6; i++){
         color += hex[Math.floor(Math.random() * 16)];
     }
     return color;
-}
+};
 console.log(randomColor());
 
 let intervalId;
@@ -15,7 +15,7 @@ const startChangingColor = function(){
     }
     
     function changeBgColor(){
-        document.body.style.backgroundColor = randomColor()
+        document.body.style.backgroundColor = randomColor();
     }
 }
 
