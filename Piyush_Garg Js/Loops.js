ff210cc91cc757a20924280de9b42e44adcca89f
Deepak Yadav <147043582+DeepakY-Git@users.noreply.github.com>
2025-01-07 14:13:33 +0000

    loops and game remaining

---
+++ Piyush_Garg Js/Loops.js
@@ -0,0 +1,29 @@
+// For Loop --> If u Know How many times u 
+
+for(let i = 1; i <= 10; i = i + 1){
+    console.log("Abc" + 2*i)
+}
+
+// While --> You know when to stop condition
+
+let ip = 0
+let house = 100
+
+while(ip!=100){
+    ip = ip + 50;
+    console.log('Step taken' + ip)
+}
+
+// Do While Loop --> First runs code then check condition
+
+let ip_ = 0
+let house_ = 100
+
+do{
+    ip_ = ip_ + 50;
+    console.log('Step taken' + ip_)
+}while(ip_ <= house_)
+
+// Guess the input
+
+
